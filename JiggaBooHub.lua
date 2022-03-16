8eefa2ff18b2966724a04a2a77be9c0f77e3af2d
AjTheProd <59293028+AjTheProd@users.noreply.github.com>
2022-03-16 18:19:59 -0400

    Update JiggaBooHub.lua

--- JiggaBooHub.lua
+++ JiggaBooHub.lua
@@ -1,7 +1,7 @@
 local PlaceId = game.PlaceId
 
 if PlaceId == 6284583030 then
-  loadstring(game:HttpGet"https://raw.githubusercontent.com/AjTheProd/PSX/main/Script.lua")()
+  	loadstring(game:HttpGet"https://raw.githubusercontent.com/AjTheProd/PSX/main/Script.lua")()
 elseif PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/AjTheProd/BloxFruits/main/Scripts.lua"))()
 else
