910e36e5d45279333caaa4ba94960c295af1b75b
AjTheProd <59293028+AjTheProd@users.noreply.github.com>
2022-03-16 18:19:02 -0400

    Create JiggaBooHub.lua

---
+++ JiggaBooHub.lua
@@ -0,0 +1,11 @@
+local PlaceId = game.PlaceId
+
+if PlaceId == 6284583030 then
+  loadstring(game:HttpGet"https://raw.githubusercontent.com/AjTheProd/PSX/main/Script.lua")()
+elseif PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/AjTheProd/BloxFruits/main/Scripts.lua"))()
+else
+	game.Players.LocalPlayer:kick("สคริปไม่มีเกมนี้ ไอ้เวร")
+	wait(1)
+	game:Shutdown()
+end
